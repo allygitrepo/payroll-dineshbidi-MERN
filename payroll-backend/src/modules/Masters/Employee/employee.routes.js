f3276a9a361c33019b3e5acb70f3aacb3713e196
SarvaiyaPrashant2242005 <prashanthere90@gmail.com>
2026-06-24 15:24:54 +0530

    Missing Information

--- payroll-backend/src/modules/Masters/Employee/employee.routes.js
+++ payroll-backend/src/modules/Masters/Employee/employee.routes.js
@@ -7,6 +7,7 @@ const authenticateJWT = require("../../../middlewares/auth.middleware");
 // All routes are protected by JWT authentication
 router.post("/", authenticateJWT, EmployeeController.create);
 router.get("/company/:companyId", authenticateJWT, EmployeeController.getAll);
+router.get("/company/:companyId/missing-details", authenticateJWT, EmployeeController.getMissingDetails);
 router.get("/:id", authenticateJWT, EmployeeController.getById);
 router.put("/:id", authenticateJWT, EmployeeController.update);
 router.delete("/:id", authenticateJWT, EmployeeController.delete);
