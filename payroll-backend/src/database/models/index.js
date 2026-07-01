ed2a3369a4d0fcd56094e6b38ee85976e062d5db
SarvaiyaPrashant2242005 <prashanthere90@gmail.com>
2026-07-01 15:39:06 +0530

    added whatsappp associations

--- payroll-backend/src/database/models/index.js
+++ payroll-backend/src/database/models/index.js
@@ -31,6 +31,8 @@ const LeavePolicy = require("../../modules/Setup/LeaveMaster/leavePolicy.model")
 const LeaveBalance = require("../../modules/Attendance/Leave/leaveBalance.model");
 const LeaveTransaction = require("../../modules/Attendance/Leave/leaveTransaction.model");
 const EmployeeType = require("../../modules/Masters/EmployeeType/employeeType.model");
+const WhatsAppInstance = require("../../modules/Setup/WhatsApp/whatsAppInstance.model");
+const WhatsAppTemplate = require("../../modules/Setup/WhatsApp/whatsappTemplate.model");
 
 // Registry object to hold Sequelize, Sequelize constructors, and model definitions
 const db = {};
@@ -68,6 +70,8 @@ db.LeavePolicy = LeavePolicy;
 db.LeaveBalance = LeaveBalance;
 db.LeaveTransaction = LeaveTransaction;
 db.EmployeeType = EmployeeType;
+db.WhatsAppInstance = WhatsAppInstance;
+db.WhatsAppTemplate = WhatsAppTemplate;
 
 
 // Model associations
@@ -560,5 +564,24 @@ db.LeaveType.hasMany(db.LeaveRequest, {
     onDelete: "CASCADE",
 });
 
+
+// WhatsApp relationships
+db.Company.hasOne(db.WhatsAppInstance, {
+    foreignKey: "company_id",
+    as: "whatsapp_instance",
+});
+db.WhatsAppInstance.belongsTo(db.Company, {
+    foreignKey: "company_id",
+    as: "company",
+});
+
+db.Company.hasMany(db.WhatsAppTemplate, {
+    foreignKey: "company_id",
+    as: "whatsapp_templates",
+});
+db.WhatsAppTemplate.belongsTo(db.Company, {
+    foreignKey: "company_id",
+    as: "company",
+});
 
 module.exports = db;
